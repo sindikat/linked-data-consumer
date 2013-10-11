b1c8825b6ad721210d91cb0faf8b86f163fb1375
sindikat <sindikat@mail36.net>
2013-10-12 01:40:33 +0600

    Simple dereference of URIs
    Uses content negotiation through HTTP Accept headers
    
    --HG--
    extra : convert_revision : c14166e052374ae4425c727bf98396e96905535c

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from rdflib import Graph, URIRef
 from posixpath import join
+from urllib2 import urlopen, Request, URLError
 
 DATAFILE = 'data.ttl'
 HTTP = 'http://'
@@ -14,6 +15,12 @@ g.parse(DATAFILE, format='n3')
 
 def get_uri(uri):
     '''get uri as subject using sparql query'''
+    global g # dirty
+
+    load_graph = load_rdf(uri)
+    g += load_graph
+    save(g)
+
     query_template_subject = '''select ?p ?o
     where {{
     <{0}> ?p ?o .
@@ -76,3 +83,33 @@ def sparql_query(graph, query):
     final = result_to_xss(result)
 
     return final
+
+def load_rdf(uri):
+    '''Accepts URI, returns Graph'''
+
+    # headers are taken from
+    # http://richard.cyganiak.de/blog/2008/03/what-is-your-rdf-browsers-accept-header/
+    headers = 'application/rdf+xml, application/xhtml+xml;q=0.3, text/xml;q=0.2, application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3, text/rdf+n3;q=0.5, application/x-turtle;q=0.2, text/turtle;q=1'
+
+    new_graph = Graph()
+
+    req = Request(uri)
+    req.add_header('Accept', headers)
+
+    # rewrite the following using requests library
+    try:
+        resp = urlopen(req)
+        content = resp.read()
+        new_graph.parse(data=content)
+    except URLError:
+        pass
+
+    return new_graph
+
+def save(graph):
+    '''Serialize graph to Turtle.
+
+    SIDE EFFECTS
+    '''
+    graph.serialize('data.ttl', format='n3')
+    return None
